04551909a9a59ca2a4acec5b2a293acc62a42816
Antall Nguyen <nguyena7@udayton.edu>
2022-03-16 00:52:22 -0400

    typeracer hax lol

---
+++ main.py
@@ -0,0 +1,35 @@
+import pyautogui
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+DRIVER_PATH = "./chromedriver.exe"
+URL = "https://play.typeracer.com/"
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+# TODO: SOMEHOW WEBSCRAPE TYPERACER OR SOME OTHER SHITTY TYPE RACING WEBSITE THAT DOESN'T MAKE IT A PAIN IN THE ASS TO SCRAPE THE TEXT.
+driver = webdriver.Chrome(service=Service(executable_path=DRIVER_PATH), options=options)
+driver.get(URL)
+
+# I ONLY DOING THIS IN PRACTICE MODE - I AINT A SCUMBAG. Unless........<.<
+try:
+    practice_btn = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gwt-uid-2"]/a')))
+    practice_btn.click()
+except Exception as e:
+    print(e)
+
+sentence = ""
+try:
+    text = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gwt-uid-20"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div')))
+    span_list = text.find_elements(By.TAG_NAME, "span")
+    for span in span_list:
+        sentence+=span.get_attribute("innerHTML") # Doing "span.text" no workie so I do "get_attribute" instead...
+except Exception as e:
+    print(e)
+
+sleep(4.5) # give me some to pick the window cuh
+pyautogui.typewrite(sentence, interval=0.025)
