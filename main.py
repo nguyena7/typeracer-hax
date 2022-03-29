96c239e9aae9b11b465ed10a145b9490490ae6d3
Antall Nguyen <nguyena7@udayton.edu>
2022-03-28 21:42:03 -0400

    idk im bored

--- main.py
+++ main.py
@@ -10,26 +10,20 @@ DRIVER_PATH = "./chromedriver.exe"
 URL = "https://play.typeracer.com/"
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-
-# TODO: SOMEHOW WEBSCRAPE TYPERACER OR SOME OTHER SHITTY TYPE RACING WEBSITE THAT DOESN'T MAKE IT A PAIN IN THE ASS TO SCRAPE THE TEXT.
 driver = webdriver.Chrome(service=Service(executable_path=DRIVER_PATH), options=options)
 driver.get(URL)
+sentence = ""
 
 try:
-    # I ONLY DOING THIS IN PRACTICE MODE - I AINT A SCUMBAG. Unless........<.<
+    # Grab practice button. I ONLY DOING THIS IN PRACTICE MODE - I AINT A SCUMBAG. Unless........<.<
     practice_btn = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gwt-uid-2"]/a')))
     practice_btn.click()
-except Exception as e:
-    print(e)
-
-sentence = ""
-try:
+    # Grab text box
     text = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gwt-uid-20"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div')))
     span_list = text.find_elements(By.TAG_NAME, "span")
     for span in span_list:
         sentence+=span.get_attribute("innerHTML") # Doing "span.text" no workie so I do "get_attribute" instead...
 except Exception as e:
     print(e)
-
 sleep(4.5) # give me some to pick the window cuh
 pyautogui.typewrite(sentence, interval=0.025)
